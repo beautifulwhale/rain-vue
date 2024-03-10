aba63d4fe068ec3b2a7be7da1e81d91e0d4d38a1
听孙燕姿的Jay <1928999500@qq.com>
2024-03-10 14:01:04 +0800

    feat: 添加了上下文节点操作

--- 04编译器/transform.js
+++ 04编译器/transform.js
@@ -1,16 +1,24 @@
 // 转化ast
 export function traverseNode(ast, content) {
   const currentNode = ast;
+  content.currentNode = ast;
 
   const transforms = content.nodeTransforms;
   for (let i = 0; i < transforms.length; i++) {
     transforms[i](currentNode, content);
+
+    if (!content.currentNode) return;
   }
 
   // 深度遍历每一个ast
   const children = currentNode.children;
   if (children) {
-    currentNode.children.forEach(c => traverseNode(c, content));
+    for (let i = 0; i < children.length; i++) {
+      content.parent = currentNode;
+      content.childIndex = i;
+
+      traverseNode(children[i], content);
+    }
   }
 }
 
@@ -18,18 +26,31 @@ export function traverseNode(ast, content) {
 // 解耦traverseNode函数
 export function transform(ast) {
   const content = {
+    currentNode: null,
+    childIndex: 0,
+    parent: null,
+    replaceElement(node) {
+      this.parent.children[this.childIndex] = node;
+      this.currentNode = node;
+    },
     nodeTransforms: [
       transformElement,
-      transformText
+      // transformText
     ]
   };
 
   traverseNode(ast, content);
 }
 
-function transformElement(node) {
-  if (node.type === 'Element' && node.tag === 'p') {
-    node.tag = 'h1';
+function transformElement(node, content) {
+  // if (node.type === 'Element' && node.tag === 'p') {
+  //   node.tag = 'h1';
+  // }
+  if (node.type === 'Text') {
+    content.replaceElement({
+      type: 'Element',
+      tag: 'span'
+    });
   }
 }
 
